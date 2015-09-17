b78ccfa09314fd913fc0b9b055ee1b78f2d13e72
phejny <ph@towns,cz>
2015-09-18 01:08:38 +0200

    [PH] opravy

---
+++ fakeserver.php
@@ -0,0 +1,14 @@
+<?php
+
+    header('content-type: application/jsonp');
+
+    if($_GET['callback'])
+        $callback=$_GET['callback'];
+    else
+        $callback='callback';
+
+
+    echo($callback.'(');
+    readfile('objects.json');
+    echo(');');
+?>
