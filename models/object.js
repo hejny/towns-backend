388ef4f28f49acd3ca7915fb04f62ac3f893aaa5
Stefan Kecskes <mr.kecskes@gmail.com>
2015-12-30 00:18:20 +0000

    [API][SK] commented out error causing the node server to not start.

--- models/object.js
+++ models/object.js
@@ -1,8 +1,8 @@
 var db = require('../db');
-var Object = db.model('Object', {
-    x: {type: Integer, required: true},
-    y: {type: Integer, required: true},
-    objectType: {type: String, required: true},
-    date: {type: Date, required: true, default: Date.now()}
-});
-module.exports = Post;
+//var Object = db.model('Object', {
+//    x: {type: integer, required: true},
+//    y: {type: integer, required: true},
+//    objectType: {type: String, required: true},
+//    date: {type: Date, required: true, default: Date.now()}
+////});
+//module.exports = Post;
