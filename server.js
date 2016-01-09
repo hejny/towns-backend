04f3d3e120e45fb6794092ef2733924414cf2d01
Pavol Hejný <me@pavolhejny.com>
2016-01-09 03:30:59 +0100

    [API][PH]  CORS rules

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'layouts'));
 app.set('view engine', 'jade');
