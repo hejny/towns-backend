c275789494c4caa11673fd9b2481f9ce8fd7da1b
Pavol Hejný <me@pavolhejny.com>
2016-01-10 02:32:10 +0100

    [API][PH]  Schema of object

--- models/object.js
+++ models/object.js
@@ -11,11 +11,12 @@ var schema = new db.Schema({
     locale: {type: String, default: "cs"},
     design: {
         type: {type: String, default: "model"},
-        data: {
-            particles: Array,//[PH] This is specification for only one type of data - model. In future there will be other types with different specifications.
+        data: db.Schema.Types.Mixed
+        /*{//[PH] This is specification for only one type of data - model. In future there will be other types with different specifications.
+            particles: Array,
             rotation: {type: Number, default: 0},
             size: {type: Number, default: 1}
-        }
+        }*/
     },
     content: {
         type: {type: String, default: "markdown"},
