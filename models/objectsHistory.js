20461d251797ba4d43386b31a11ce3fbc003559a
Pavol Hejný <me@pavolhejny.com>
2016-01-14 13:31:33 +0100

    [API][PH]  Object db.Schema

--- models/objectsHistory.js
+++ models/objectsHistory.js
@@ -1,37 +1,33 @@
 var db = require('./db');
 
 
-
 /*
-Changes in this db.Schema should be applied in all object schema files!
+ Changes in this db.Schema should be applied in all object schema files!
 
 
-ObjectsPrototypesHistory
-←
-ObjectsPrototypes
-→
-Objects - current
-→
-ObjectsHistory
+ ObjectsPrototypesHistory
+ ←
+ ObjectsPrototypes
+ →
+ Objects
+ →
+ ObjectsHistory - current
 
-*/
-var schema = new db.Schema({
+ */
+var historySchema = new db.Schema({
+    _currentId: { type: String, required:true },
     version: {type: Number, required: true, default: 1},
     name: String,
     type: {type: String},
     subtype: String,
     x: {type: Number, required: true},
     y: {type: Number, required: true},
-    start_time: {type: Date, required: true, default: Date.now},
+    start_time: {type: Date, required: true},
+    stop_time: {type: Date, required:true, default: Date.now},
     locale: {type: String, default: "cs"},
     design: {
         type: {type: String, default: "model"},
         data: db.Schema.Types.Mixed
-        /*{//[PH] This is specification for only one type of data - model. In future there will be other types with different specifications.
-            particles: Array,
-            rotation: {type: Number, default: 0},
-            size: {type: Number, default: 1}
-        }*/
     },
     content: {
         type: {type: String, default: "markdown"},
@@ -44,7 +40,9 @@ var schema = new db.Schema({
     },
     actions: Array,
     owner: {type: String, required: true, default: "admin"}
+}, {
+    collection: 'objectsHistory'
 });
 
-var object = db.model('objects', schema);
-module.exports = object;
+var objectsHistory = db.model('objectsHistory', historySchema, 'objectsHistory');
+module.exports = objectsHistory;
