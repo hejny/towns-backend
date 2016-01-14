6ba0f557c7e0e7942f4c2868bfb88a918269ad13
Pavol Hejný <ph@towns.cz>
2016-01-14 13:31:33 +0100

    [API][PH]  Object db.Schema

--- models/objectsPrototype.js
+++ models/objectsPrototype.js
@@ -1,13 +1,27 @@
 var db = require('./db');
 
+
+/*
+ Changes in this db.Schema should be applied in all object schema files!
+
+
+ ObjectsPrototypesHistory
+ ←
+ ObjectsPrototypes - current
+ →
+ Objects
+ →
+ ObjectsHistory
+
+ */
 var prototypeSchema = new db.Schema({
     name: String,
     type: {type: String, required: true},
     subtype: String,
     locale: {type: String, default: "cs"},
     design: {
-        type: {type: String},
-        data: {type: String}
+        type: {type: String, default: "model"},
+        data: db.Schema.Types.Mixed
     },
     content: {
         type: {type: String, default: "markdown"},
