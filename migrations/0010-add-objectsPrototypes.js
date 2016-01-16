8cac0a6dacdaf6262866ad5111568cdc71024ff8
Pavol Hejný <ph@towns.cz>
2016-01-16 03:25:23 +0100

    [API][PH]  Migrations / terrains

--- migrations/0010-add-objectsPrototypes.js
+++ migrations/0010-add-objectsPrototypes.js
@@ -9,30 +9,245 @@ exports.up = function (db, next) {
     });
 
     objectsPrototypes.insert([
+
+        //==============================================================================================================building
+
+        {
+            name: "Kamenný domek",
+            type: "building",
+            subtype: "main",
+            design: {
+                type: "model",
+                data: {
+                    particles: [
+                        {
+                            shape:{
+                                type: 'prism',
+                                n:4
+                            },
+                            color: "#cccccc",
+                            position: {x:0,y:0,z:0},
+                            size: {x:50,y:50,z:50},
+                            rotation: {"xy":0,"xz":0}
+
+                        },{
+                            shape:{
+                                type: 'prism',
+                                n:4,
+                                top:0
+                            },
+                            color: "#cccccc",
+                            position: {x:0,y:0,z:100},
+                            size: {x:50,y:50,z:40},
+                            rotation: {"xy":0,"xz":0}
+
+                        }
+                    ]
+                }
+
+            }
+
+        },{
+            name: "Kamenná pyramida",
+            type: "building",
+            subtype: "block",
+            design: {
+                type: "model",
+                data: {
+                    particles: [
+                        {
+                            shape:{
+                                type: 'prism',
+                                n:4,
+                                top:1
+                            },
+                            color: "#cccccc",
+                            position: {x:0,y:0,z:0},
+                            size: {x:50,y:50,z:50},
+                            rotation: {"xy":0,"xz":0}
+
+                        }
+                    ]
+                }
+
+            }
+
+        },{
+            name: "Kamenný ježek",
+            type: "building",
+            subtype: "wall",
+            design: {
+                type: "model",
+                data: {
+                    particles: [
+                        {
+                            shape:{
+                                type: 'prism',
+                                n:3,
+                                top:0.1
+                            },
+                            color: "#cccccc",
+                            position: {x:0,y:0,z:0},
+                            size: {x:40,y:40,z:40},
+                            rotation: {"xy":30,"xz":0}
+
+                        }
+                    ]
+                }
+
+            }
+
+        },
+
+
+        //==============================================================================================================terrain
         {
             "type": "terrain",
+            "name": "Terén",
             "design": {
                 "type": "terrain",
-                "data": 0
-            },
-            "owner": "5126bc054aed4daf9e2ab772"
-        }, {
-            "name": "Ambasada",
-            "type": "building",
-            "subtype": "main",
-            "locale": "cs",
+                "data":{
+                    image:1,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
             "design": {
-                "type": "model",
-                "data": {}
-            },
-            "properties": {
-                "strength": 5,
-                "defense": 3,
-                "speed": 10
-            },
-            "actions": ["attack", "defense", "dismantle"],
-            "owner": "5126bc054aed4daf9e2ab772"
-        }, {
+                "type": "terrain",
+                "data":{
+                    image:2,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:3,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:4,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:5,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:6,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:7,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:8,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:9,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:10,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:11,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:12,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:13,
+                    size: 1
+                }
+            }
+        },{
+            "type": "terrain",
+            "name": "Terén",
+            "design": {
+                "type": "terrain",
+                "data":{
+                    image:14,
+                    size: 1
+                }
+            }
+        },
+
+
+        //==============================================================================================================story
+
+
+        {
             "name": "Příběh",
             "type": "story",
             "locale": "cs",
@@ -42,6 +257,9 @@ exports.up = function (db, next) {
             },
             "owner": "5126bc054aed4daf9e2ab772"
         }
+
+
+        //==============================================================================================================
     ], next);
 
 };
