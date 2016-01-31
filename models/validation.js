e33da758fe7aaa709c12740d869f2e423b94aa03
Stefan Kecskes <mr.kecskes@gmail.com>
2016-01-31 22:40:36 +0000

    progressing validator

--- models/validation.js
+++ models/validation.js
@@ -1,11 +1,14 @@
+var validator = require('validator');
+
 // checks alpha numeric strings + space
-function isAlphanumeric(value) {
+function isAlphanumericstring(value) {
+    //return validator.isAlphanumeric(value);
     return /^[a-zA-Z0-9 ]+$/i.test(value);
 }
 
 // checks valid Type
-function isValidType(value) {
-    return /building|terrain|story/i.test(value);
+function typesOfObjects(value) {
+    return validator.isIn(value, ['building', 'terrain', 'story']);
 }
 
 // checks that locale has only alphabet characters
@@ -18,10 +21,19 @@ function hasTwoCharacters(value) {
     return value.length == 2 || value.length == 0;
 }
 
-var validation = {
-    alphanumeric: [isAlphanumeric, '{VALUE} is not alphanumeric'],
-    validType: [isValidType, '{VALUE} is not valid type!'],
-    validLocale: [{ 'validator': isAlphabetic, msg: '{VALUE} is not valid locale! Locale must be in ISO 3166-1 alpha-2 format.'}, {'validator': hasTwoCharacters, msg: '{VALUE} must be 2 characters long'}]
+function isMongoId(value) {
+    // TODO: temporarily allow admin as value
+    return validator.isMongoId(value) || value == "admin";
+}
+
+var is = {
+    validObjectName: [isAlphanumericstring, '{VALUE} is not alphanumeric'],
+    validObjectType: [typesOfObjects, '{VALUE} is not valid TYPE!'],
+    validObjectLocale: [
+        { 'validator': isAlphabetic, msg: '{VALUE} is not valid locale! Locale must be in ISO 3166-1 alpha-2 format.'},
+        {'validator': hasTwoCharacters, msg: '{VALUE} must be 2 characters long'}
+    ],
+    validOwnerId: [isMongoId, '{VALUE} is not a valid Owner Id']
 };
 
-module.exports = validation;
+module.exports = is;
