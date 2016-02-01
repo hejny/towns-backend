bd6f6b22e45ebf991b581ffdba8d38da1ea667af
Stefan Kecskes <mr.kecskes@gmail.com>
2016-02-01 00:55:29 +0000

    progressing validator on objects

--- models/object.js
+++ models/object.js
@@ -3,11 +3,8 @@
 var db = require('./db');
 var is = require('./validation');
 
-
-
 /*
 Changes in this db.Schema should be applied in all object schema files!
-
 
 ObjectsPrototypesHistory
 ←
@@ -19,39 +16,67 @@ ObjectsHistory
 
 */
 var schema = new db.Schema({
-    _prototypeId: { type: String, required:true },
-    version: {type: Number, required: true, default: 0},
+    _prototypeId: {
+        type: String,
+        required:true,
+        trim: true,
+        validate: is.validObjectName
+    },
+    version: {
+        type: Number,
+        required: true,
+        default: 0,
+        validate: is.validObjectVersion
+    },
     name: {
         type: String,
         minlength: 1,
         maxlength: 64,
         trim: true,
-        validate: is.alphanumeric
+        validate: is.validObjectName
     },
     type: {
         type: String,
         required: true,
-        validate: is.validType
+        validate: is.validObjectType
     },
     subtype: {
         type: String
+    },
+    x: {
+        type: Number,
+        required: true,
+        validate: is.validObjectCoordinate
+    },
+    y: {
+        type: Number,
+        required: true,
+        validate: is.validObjectCoordinate
+    },
+    start_time: {
+        type: Date,
+        required: true,
+        default: Date.now,
+        validate: is.validDate
     },
     locale: {
         type: String,
         trim: true,
         default: "cs",
-        validate: is.validLocale
+        validate: is.validObjectLocale
     },
-    x: {type: Number, required: true},
-    y: {type: Number, required: true},
-    start_time: {type: Date, required: true, default: Date.now},
     design: {
         type: {type: String, default: "model"},
         data: db.Schema.Types.Mixed
+        /*{//[PH] This is specification for only one type of data - model. In future there will be other types with different specifications.
+            particles: Array,
+            rotation: {type: Number, default: 0},
+            size: {type: Number, default: 1}
+        }*/
     },
     content: {
-        type: {type: String, default: "markdown", trim: true},
-        data: String
+        type: {type: String, default: "markdown"},
+        data: {type: String}
     },
     properties: {
         strength: {type: Number},
@@ -59,7 +84,12 @@ var schema = new db.Schema({
         speed: {type: Number}
     },
     actions: Array,
-    owner: {type: String, required: true, default: "admin"}
+    owner: {
+        type: String,
+        required: true,
+        default: "admin",
+        validate: is.validOwnerId
+    }
 });
 
 var object = db.model('objects', schema);
