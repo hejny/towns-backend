9bdf0e8cfeb64d6b257802618c1486280e2388ff
Stefan Kecskes <mr.kecskes@gmail.com>
2016-02-04 11:21:03 +0000

    refactoring index handler

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var indexHandler = require('../handlers/indexHandler');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Towns 5 API' });
-});
+router.get('/', indexHandler.home);
 
 module.exports = router;
