97a5792671fcba443d5032d2363562151400d1be
Stefan Kecskes <mr.kecskes@gmail.com>
2016-02-05 15:01:45 +0000

    tests for index homepage

---
+++ test/objects.js
@@ -0,0 +1,44 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+
+describe('Routing', function () {
+
+    this.timeout(15000);
+    var url = 'http://localhost:3000';
+
+    // within before() you can run all the operations that are needed to setup your tests (eg. create DB connection)
+    before(function (done) {
+        // do something before done
+        done();
+    });
+
+    describe('Object', function () {
+        this.timeout(15000);
+
+        //it('should correctly update an existing account', function (done) {
+        //    var body = {
+        //        firstName: 'JP',
+        //        lastName: 'Berd'
+        //    };
+        //    request(url)
+        //        .post('/api/profiles/vgheri')
+        //        .send(body)
+        //        .expect('Content-Type', /json/)
+        //        .expect(200) //Status code
+        //        .end(function (err, res) {
+        //            if (err) {
+        //                throw err;
+        //            }
+        //            // Should.js fluent syntax applied
+        //            res.body.should.have.property('_id');
+        //            res.body.firstName.should.equal('JP');
+        //            res.body.lastName.should.equal('Berd');
+        //            res.body.creationDate.should.not.equal(null);
+        //            done();
+        //        });
+        //});
+    });
+
+
+});
