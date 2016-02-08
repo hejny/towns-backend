f948a65513a1c2d71d9d0661992eac7e503974d9
Stefan Kecskes <mr.kecskes@gmail.com>
2016-02-08 10:11:38 +0000

    refactoring - extracting server variables to config files

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ var mongo = require('../config/mongo').mongoDb;
 
 try
 {
-   mongoose.connect('mongodb://'+mongo.host+':'+mongo.port+'/'+mongo.db, function() {
+   mongoose.connect(mongo.protocol+'//'+mongo.host+':'+mongo.port+'/'+mongo.db, function() {
       console.log('mongodb connected');
    });
 } catch(e)
