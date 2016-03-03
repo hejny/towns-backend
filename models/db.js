81127f7b1602b0d51f7ae979c561d2401818b304
Stefan Kecskes <mr.kecskes@gmail.com>
2016-03-03 23:55:04 +0000

    some more test tweaks - environment variables for test index and mocha tests

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 require('mongoose-double')(mongoose);
-if( process.env.NODE_ENV === 'test') {
+if( process.env.NODE_ENV == 'test') {
    var mongo = require('../config/mLab').mongoDb;
 } else {
    var mongo = require('../config/mongo').mongoDb;
@@ -10,7 +10,7 @@ try
 {
    mongoose.connect(mongo.protocol+'//'+mongo.username+':'+mongo.password+'@'+mongo.host+':'+mongo.port+'/'+mongo.db, function() {
       console.log('mongodb connected');
-      console.log();
+      console.log(mongo);
    });
 } catch(e)
 {
