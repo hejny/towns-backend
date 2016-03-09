ccb571b1299e71c884d2cbbea01f2cc4ec0f2ea6
Stefan Kecskes <mr.kecskes@gmail.com>
2016-03-09 01:39:03 +0000

    progressing auth - fixing DB connection without user

--- migrations/0025-add-users.js
+++ migrations/0025-add-users.js
@@ -5,17 +5,22 @@ exports.up = function(db, next){
 
     var users = db.collection('users');
     users.createIndex({
-        names: 1,
         version: 1
     });
     users.insert([
         {
-            "version": "0",
+            "version": 0,
             "names": {
-                "username": "testuser"
+                "username": "testuser",
+                "name": "",
+                "surname": "",
+                "email": ""
             },
             "login_methods": {
-                "password": "$2a$08$E39.h5.BhrVYkeScsWffWOpLNqDjVhLx9kfmtCyLZ5CoZzLod7BS2" //password is "password"
+                "password": "$2a$08$E39.h5.BhrVYkeScsWffWOpLNqDjVhLx9kfmtCyLZ5CoZzLod7BS2",
+                "google": "",
+                "facebook": "",
+                "twitter": ""
             },
             "contacts": [],
             "user_roles": [],
--- models/db.js
+++ models/db.js
@@ -5,17 +5,16 @@ if( process.env.NODE_ENV == 'test') {
 } else {
    var mongo = require('../config/mongo').mongoDb;
 }
-
+var credentials = (mongo.username != "" && mongo.password != "")? mongo.username+':'+mongo.password : "";
 try
 {
-   mongoose.connect(mongo.protocol+'//'+mongo.username+':'+mongo.password+'@'+mongo.host+':'+mongo.port+'/'+mongo.db, function() {
-      //console.log('mongodb connected');
+   mongoose.connect(mongo.protocol+'//'+credentials+'@'+mongo.host+':'+mongo.port+'/'+mongo.db, function() {
+      console.log("Connected to mongoDB @ "+mongo.host);
       //console.log(mongo);
    });
 } catch(e)
 {
    console.log(e);
 }
-console.log("Connected to mongoDB @ "+mongo.host);
 
 module.exports = mongoose;
