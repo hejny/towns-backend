7a4d58368923d51c1cc098d05b5a55d21e8f9d64
Stefan Kecskes <mr.kecskes@gmail.com>
2016-03-16 17:28:42 +0000

    progressing users creation - tests

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -14,43 +14,62 @@ var userController = {};
  * @param res
  */
 userController.createUser = function (req, res) {
-    if(!req.body.hasOwnProperty('profile') || req.body.profile.username == null || req.body.profile.username == "" ) {
-        res.status(400).json({
+
+    if (!req.body.hasOwnProperty('profile') || req.body.profile.username == null || req.body.profile.username == "") {
+        return res.status(400).json({
             "status": "error",
             "message": [{
                 param: "profile.username",
-                msg: "Username is required",
+                msg: "required",
                 val: ""
             }]
         });
     }
 
-    if(!req.body.hasOwnProperty('login_methods') ||  req.body.login_methods.password == null || req.body.login_methods.password == "") {
-        res.status(400).json({
+    if (!req.body.hasOwnProperty('login_methods') || req.body.login_methods.password == null || req.body.login_methods.password == "") {
+        return res.status(400).json({
             "status": "error",
             "message": [{
                 param: "login_methods.password",
-                msg: "Password is required",
+                msg: "required",
                 val: ""
             }]
         });
     }
 
     // check if user exist and if not then create it.
-    UserModel.findOne({"names.username": req.body.profile.username}, function( err, user) {
+    UserModel.findOne({"profile.username": req.body.profile.username}, function (err, user) {
         if (err) {
-            return next(err);
+            console.log(err);
+            return res.status(400).json({
+                "status": "error",
+                "message": [{
+                    param: "profile.username",
+                    msg: "Problem creating new users",
+                    val: "" + req.body.profile.username
+                }]
+            });
         }
         if (user == null) {
             bcrypt.hash(req.body.login_methods.password, 10, function (bcryptError, hash) {
-                if (bcryptError) { throw next(bcryptError) }
+                if (bcryptError) {
+                    console.log(bcryptError);
+                    return res.status(400).json({
+                        "status": "error",
+                        "message": [{
+                            param: "login_methods.password",
+                            msg: "Problem saving password",
+                            val: "" + req.body.login_methods.password
+                        }]
+                    });
+                }
 
-                newUser = new UserModel({
+                var newUserJson = {
                     "profile": {
                         "username": req.body.profile.username,
                         "name": (req.body.profile.hasOwnProperty('name') ? req.body.profile.name : ""),
                         "surname": (req.body.profile.hasOwnProperty('surname') ? req.body.profile.surname : ""),
-                        "birthday": (req.body.profile.hasOwnProperty('birthday') ? req.body.profile.birthday : ""),
+                        "birthday": (req.body.profile.hasOwnProperty('birthday') ? req.body.profile.birthday : Date.now()),
                         "description": (req.body.profile.hasOwnProperty('description') ? req.body.profile.description : ""),
                         "image": (req.body.profile.hasOwnProperty('image') ? req.body.profile.image : ""),
                         "email": (req.body.profile.hasOwnProperty('email') ? req.body.profile.email : "")
@@ -58,31 +77,40 @@ userController.createUser = function (req, res) {
                     "login_methods": {
                         "password": hash
                     }
-                });
+                };
+                if (req.body.hasOwnProperty('language')) {
+                    newUserJson.language = req.body.language;
+                }
 
+                newUser = new UserModel(newUserJson);
                 newUser.save(function (saveError, savedUser) {
                     if (saveError) {
                         var errMessage = [];
+                        console.log(saveError);
                         for (var errName in saveError.errors) {
-                            errMessage.push({param: err.errors[errName].path, msg: err.errors[errName].kind, value: err.errors[errName].value});
+                            errMessage.push({
+                                param: saveError.errors[errName].path,
+                                msg: saveError.errors[errName].kind,
+                                val: ""+saveError.errors[errName].value
+                            });
                         }
                         return res.status(400).json({
                             "status": "error",
                             "message": errMessage
                         });
                     }
-                    res.status(201).json({
+                    return res.status(201).json({
                         "status": "ok",
-                        "objectId": savedUser._id
+                        "userId": savedUser._id
                     });
                 })
             })
         } else {
-            res.status(400).json({
+            return res.status(400).json({
                 "status": "error",
                 "message": [{
                     param: "username",
-                    msg: "Username is already registered",
+                    msg: "taken",
                     val: "" + req.body.username
                 }]
             });
@@ -93,7 +121,7 @@ userController.createUser = function (req, res) {
 };
 
 /**
- * Returns user details of authorized user
+ * Returns user details of authorized user (/users/me)
  * @param req
  * @param res
  */
@@ -104,7 +132,7 @@ userController.getUser = function (req, res) {
     var token = req.headers['x-auth'];
     var auth = jwt.decode(token, config.secretKey);
 
-    UserModel.findOne({'names.username': auth.username}, function( err, user) {
+    UserModel.findOne({'profile.username': auth.username}, function (err, user) {
         if (err) {
             return res.status(400).json({
                 "status": "error",
---
+++ test/users.js
@@ -0,0 +1,171 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var config = require('../config/server').server;
+var UserModel = require('../models/user');
+
+describe('Users', function () {
+
+    this.timeout(15000);
+    var url = config.protocol + '//' + config.hostname + (config.port ? ':' + config.port : '');
+
+    // within before() you can run all the operations that are needed to setup your tests (eg. create DB connection)
+    before(function (done) {
+        // do something before done
+        done();
+    });
+
+    describe('Creation of new user', function () {
+        this.timeout(15000);
+
+        it('should fail when username is missing', function (done) {
+            var json = {
+                'profile': {
+                    'username': ''
+                },
+                'login_methods': {
+                    'password': 'password'
+                }
+            };
+
+            request(url)
+                .post('/users')
+                .send(json)
+                .expect('Content-Type', /json/)
+                .expect(400) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // Should.js fluent syntax applied
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.should.have.property('message');
+                    res.body.message[0].param.should.equal('profile.username');
+                    res.body.message[0].msg.should.equal('required');
+                    res.body.message[0].val.should.equal('');
+                    done();
+                });
+
+        });
+
+        it('should fail when password is missing', function (done) {
+            var json = {
+                'profile': {
+                    'username': 'myusername'
+                },
+                'login_methods': {
+                    'password': ""
+                }
+            };
+
+            request(url)
+                .post('/users')
+                .send(json)
+                .expect('Content-Type', /json/)
+                .expect(400) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // Should.js fluent syntax applied
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.message[0].param.should.equal('login_methods.password');
+                    res.body.message[0].msg.should.equal('required');
+                    res.body.message[0].val.should.equal('');
+                    done();
+                });
+
+        });
+
+        it('should fail when language is not valid', function (done) {
+            var requestJson = {
+                'profile': {
+                    'username': 'myusernamesss'
+                },
+                'login_methods': {
+                    'password': "password"
+                },
+                'language': 'ru'
+            };
+
+            request(url)
+                .post('/users')
+                .send(requestJson)
+                .expect('Content-Type', /json/)
+                .expect(400)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.message[0].param.should.equal('language');
+                    res.body.message[0].msg.should.equal('not allowed value');
+                    res.body.message[0].val.should.equal('ru');
+                    done();
+                });
+
+        });
+
+        it('should fail when user with same username is already registered');
+
+        it('should create user successfully', function (done) {
+            var json = {
+                'profile': {
+                    'username': 'testtest123321testtest123321'
+                },
+                'login_methods': {
+                    'password': "password"
+                }
+            };
+            request(url)
+                .post('/users')
+                .send(json)
+                .expect('Content-Type', /json/)
+                .expect(201) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    // Should.js fluent syntax applied
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('ok');
+                    res.body.should.have.property('userId');
+
+
+                    //find the object and check its values
+                    UserModel.findOne({_id: res.body.userId}, function (err, saved) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        if (saved.profile.username != json.profile.username) {
+                            throw new Error("Saved user is different than the one sent to API");
+                        }
+
+                        // remove mocked object
+                        saved.remove(function (err) {
+                            if (err) {
+                                throw err;
+                            }
+
+                            done();
+                        });
+
+                    });
+
+                });
+
+        });
+    });
+
+});
