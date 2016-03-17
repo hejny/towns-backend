d38fcc13c29841794f1dc7037a7c3a19ce772169
Stefan Kecskes <mr.kecskes@gmail.com>
2016-03-17 11:11:14 +0000

    username restriction reduced to 64 chars, even that is too long progressing users creation - more tests

--- test/users.js
+++ test/users.js
@@ -51,6 +51,70 @@ describe('Users', function () {
 
         });
 
+        it('should fail when user with same username is already registered', function (done) {
+            var json = {
+                'profile': {
+                    'username': 'testuser'
+                },
+                'login_methods': {
+                    'password': 'password'
+                }
+            };
+
+            request(url)
+                .post('/users')
+                .send(json)
+                .expect('Content-Type', /json/)
+                .expect(400) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // Should.js fluent syntax applied
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.should.have.property('message');
+                    res.body.message[0].param.should.equal('profile.username');
+                    res.body.message[0].msg.should.equal('taken');
+                    res.body.message[0].val.should.equal('testuser');
+                    done();
+                });
+        });
+
+        it('should fail when username is too long', function (done) {
+            var json = {
+                'profile': {
+                    'username': '1234567890123456789012345678901234567890123456789012345678901234567890'
+                },
+                'login_methods': {
+                    'password': 'password'
+                }
+            };
+
+            request(url)
+                .post('/users')
+                .send(json)
+                .expect('Content-Type', /json/)
+                .expect(400) //Status code
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    // Should.js fluent syntax applied
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.should.have.property('message');
+                    res.body.message[0].param.should.equal('username');
+                    res.body.message[0].msg.should.equal('user defined');
+                    res.body.message[0].val.should.equal('1234567890123456789012345678901234567890123456789012345678901234567890');
+                    done();
+                });
+        });
+
         it('should fail when password is missing', function (done) {
             var json = {
                 'profile': {
@@ -115,7 +179,73 @@ describe('Users', function () {
 
         });
 
-        it('should fail when user with same username is already registered');
+        it('should fail when language is not ISO-3166 formated', function (done) {
+            var requestJson = {
+                'profile': {
+                    'username': 'myusernamesss'
+                },
+                'login_methods': {
+                    'password': "password"
+                },
+                'language': 'slovensky'
+            };
+
+            request(url)
+                .post('/users')
+                .send(requestJson)
+                .expect('Content-Type', /json/)
+                .expect(400)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.message[0].param.should.equal('language');
+                    res.body.message[0].msg.should.equal('length');
+                    res.body.message[0].val.should.equal('slovensky');
+                    done();
+                });
+        });
+
+        it('should fail when language is not one of the allowed type', function (done) {
+
+
+             //currently we support only czech and english language. bg stands for bulgaria
+             //it should fail while we don't support that language
+
+            var requestJson = {
+                'profile': {
+                    'username': 'myusernamesss'
+                },
+                'login_methods': {
+                    'password': "password"
+                },
+                'language': 'bg'
+            };
+
+            request(url)
+                .post('/users')
+                .send(requestJson)
+                .expect('Content-Type', /json/)
+                .expect(400)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.have.property('status');
+                    res.body.status.should.not.equal(null);
+                    res.body.status.should.equal('error');
+                    res.body.message[0].param.should.equal('language');
+                    res.body.message[0].msg.should.equal('not allowed value');
+                    res.body.message[0].val.should.equal('bg');
+                    done();
+                });
+
+        });
 
         it('should create user successfully', function (done) {
             var json = {
