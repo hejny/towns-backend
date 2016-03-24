e7998e9d29575cc8e7bdadbe2dcda31e20e47574
Stefan Kecskes <mr.kecskes@gmail.com>
2016-03-24 15:48:22 +0000

    refactoring middlewares

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./authMiddleware'));
+app.use(require('./controllers/middleware/auth'));
 
 
 // CONTROLLER ROUTES
-
+// -----------------
 // route for index page
 var indexRoutes = require('./routes/index');
 app.use('/', indexRoutes);
@@ -57,30 +57,9 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+// -----------------
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
-
+app.use(require('./controllers/middleware/auth'));
 
 module.exports = app;
