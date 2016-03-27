d0509725437b554e9ad24eff1a2dade07b801ee5
Stefan Kecskes <mr.kecskes@gmail.com>
2016-03-27 23:53:01 +0100

    tests for auth middleware on objects

--- routes/objects.js
+++ routes/objects.js
@@ -13,24 +13,24 @@ router.get('/', objectsController.getAll);
  * POST /objects
  * Create received resource
  */
-router.post('/', objectsController.createOne);
+router.post('/', auth.check, objectsController.createOne);
 
 /**
  * GET /objects/:id
  * Returns object with given id
  */
-router.get('/:id', objectsController.getOne);
+router.get('/:id', auth.check, objectsController.getOne);
 
 /**
  * POST /objects/:id
  * Update object with given id, according to json sent in body
  */
-router.post('/:id', objectsController.updateOne);
+router.post('/:id', auth.check, objectsController.updateOne);
 
 /**
  * DELETE /objects/:id
  * Deletes the object with given id
  */
-router.delete('/:id', objectsController.deleteOne);
+router.delete('/:id', auth.check, objectsController.deleteOne);
 
 module.exports = router;
