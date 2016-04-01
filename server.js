5b89a0739d1f1252ebbdffd405a804310d6aa165
Pavol Hejný <ph@towns.cz>
2016-04-01 13:00:55 +0200

    [PH] Cross origin bugfix

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ var bodyParser = require('body-parser');
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,HEAD");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, x-auth, Content-Type, Accept");
     next();
+});
+
+
+app.options('*',function(req, res, next) {
+    res.send('GET,POST,HEAD,DELETE');
 });
 
 
