40168e903e3146e328c09084c9c0ea853dc1de8e
Stefan Kecskes <mr.kecskes@gmail.com>
2016-04-05 01:07:09 +0100

    more amends to production deploy

--- bin/production-deploy.sh
+++ bin/production-deploy.sh
@@ -5,11 +5,12 @@ NOW=$(date +"%F_%H%M%S")
 
 # copy and symlink new build
 cp -r $PWD /var/www/omega_towns_cz/api-$NOW
-rm /var/www/omega_towns_cz/current
+rm -r /var/www/omega_towns_cz/current
 ln -s /var/www/omega_towns_cz/api-$NOW /var/www/omega_towns_cz/current
 
 # replace test .env file with staging
 cp /var/www/ci_towns_cz/dotenv/.env.production /var/www/omega_towns_cz/api-$NOW/.env
+cd /var/www/omega_towns_cz/api-$NOW && npm run mongo-migrate
 
 # restart node server
 # pm2 start /var/www/omega_towns_cz/current/bin/www --name omega-api
