5f642fe68443480ad3aaacf2ee7d8a67a8e54492
Stefan Kecskes <mr.kecskes@gmail.com>
2016-04-22 16:19:24 +0100

    GET /stories tests

---
+++ test/stories.js
@@ -0,0 +1,50 @@
+var should = require('should');
+var assert = require('assert');
+var request = require('supertest');
+var config = require('../config/server').server;
+var ObjectModel = require('../database/models/object');
+var ObjectsPrototype = require('../database/models/objectsPrototype');
+var ObjectsHistory = require('../database/models/objectsHistory');
+var UserModel = require('../database/models/user');
+
+describe('Stories', function () {
+
+    // within before() you can run all the operations that are needed to setup your tests (eg. create DB connection)
+    before(function (done) {
+        // do something before done
+        this.timeout(15000);
+        var url = config.protocol + '//' + config.hostname + (config.port ? ':' + config.port : '');
+        done();
+    });
+
+    describe('Getting list of stories from API', function () {
+
+        it('should return a list of objects', function (done) {
+
+            request(url)
+                .get('/stories')
+                .expect('Content-Type', /json/)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) {
+                        throw err;
+                    }
+
+                    res.body.should.be.instanceof(Array);
+                    for (var each in res.body) {
+                        res.body[each].should.have.property('name');
+                        res.body[each].name.should.not.equal(null);
+                        res.body[each].should.have.property('locale');
+                        res.body[each].should.have.property('type');
+                        res.body[each].type.should.equal('story');
+                        res.body[each].should.have.property('start_time');
+                        res.body[each].should.have.property('owner');
+                    }
+
+                    done();
+                });
+        });
+
+    });
+
+});
